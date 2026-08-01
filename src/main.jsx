3f52a9a6aa2317b152156209969d7e8daf234473
mkhamimhasan <mkhamim1050100@gmail.com>
2026-08-02 01:41:52 +0600

    Remove unused Inter 500 font weight

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,6 @@ import '@fontsource/orbitron/700.css';
 import '@fontsource/orbitron/800.css';
 import '@fontsource/orbitron/900.css';
 import '@fontsource/inter/400.css';
-import '@fontsource/inter/500.css';
 import '@fontsource/inter/600.css';
 import '@fontsource/inter/700.css';
 import '@fontsource/jetbrains-mono/400.css';
